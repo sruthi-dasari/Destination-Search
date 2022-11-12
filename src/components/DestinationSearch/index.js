1a4e6e8d7b72a24c5ca718d6e6b33227211a0df0
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-11-12 22:18:17 +0530

    All test cases passed but search bar not coming as expected

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,31 +1,56 @@
+import {Component} from 'react'
 import './index.css'
 import DestinationItem from '../DestinationItem'
 
-const DestinationSearch = props => {
-  const {destinationsList} = props
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
 
-  return (
-    <div className="bg-container">
-      <h1 className="main-heading">Destination Search</h1>
-      <div className="wrapper">
-        <input type="search" placeholder="Search" className="search-bar" />
-        <img
-          className="search-icon"
-          src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-          alt="search icon"
-        />
-      </div>
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
 
-      <ul className="list-container">
-        {destinationsList.map(eachDestination => (
-          <DestinationItem
-            destinationDetails={eachDestination}
-            key={eachDestination.id}
+  render() {
+    const {destinationsList} = this.props
+    const {searchInput} = this.state
+    const searchResults = destinationsList.filter(eachDestination => {
+      const destName = eachDestination.name
+      const destNameInLower = destName.toLowerCase()
+      const destNameInUpper = destName.toUpperCase()
+      return (
+        destName.includes(searchInput) ||
+        destNameInLower.includes(searchInput) ||
+        destNameInUpper.includes(searchInput)
+      )
+    })
+    return (
+      <div className="bg-container">
+        <h1 className="main-heading">Destination Search</h1>
+        <div className="wrapper">
+          <input
+            type="search"
+            value={searchInput}
+            onChange={this.onChangeSearchInput}
+            placeholder="Search"
+            className="search-bar"
           />
-        ))}
-      </ul>
-    </div>
-  )
+          <img
+            className="search-icon"
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+          />
+        </div>
+
+        <ul className="list-container">
+          {searchResults.map(eachDestination => (
+            <DestinationItem
+              destinationDetails={eachDestination}
+              key={eachDestination.id}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default DestinationSearch
